6a5c4c7ce734f4b2a2e013327928bab84bd2760c
DairaAl <dairaal27@gmail.com>
2016-11-05 00:36:13 -0600

    Create bdConexion.java

---
+++ bdConexion.java
@@ -0,0 +1,57 @@
+//Flta encabezado.
+
+package controlcalificaciones;
+
+import java.sql.*;
+import java.util.logging.*;
+import javax.swing.JOptionPane;
+
+public class bdConexion {
+
+    private Statement comando;
+    Connection con = null;
+
+    public Connection conectar() throws SQLException, InstantiationException, IllegalAccessException {
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+            //Class.forName("com,mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/dbcontrolcalificacion", "root", "");
+            comando = con.createStatement();
+            System.out.println("conexion establecida");
+        } catch (ClassNotFoundException e) {
+            System.out.println("error de conexion" + e);
+        }
+        return con;
+    }
+
+    public Connection getConexion() {
+        return con;
+    }
+
+    /**
+     * Ejecuta una consulta SQL con SELECT*
+     */
+    public ResultSet ejecutarConsulta(String instruccionQL) throws SQLException {
+        ResultSet resultado = this.comando.executeQuery(instruccionQL);
+        return resultado;
+    }
+
+    /**
+     * Ejecuta una comando DML como INSERt, UPDATE o DELETE o tambien DDL como
+     * CREATE, DROP, ALTER*
+     */
+    public int ejecutarActualizacion(String instruccionDML) throws SQLException {
+        int i;
+        i = this.comando.executeUpdate(instruccionDML);
+        return i;
+    }
+
+    public void desconectar(Connection con) {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+}
